aa5571d4d141c91d89ca32e907c916b9edf570aa
NF RIDOY <nfridoy@gmail.com>
2023-06-12 23:13:28 +0600

    bug: submit match btn

--- script.js
+++ script.js
@@ -10,9 +10,29 @@ function digit4() {
     }
 }
 // digit4();
-
+let GeneratedPinValue,digitsValue;
 function GeneratePin() {
     GeneratedPin = document.getElementById("GeneratedPin");
     GeneratedPin.value = "";
-    GeneratedPin.value = parseInt(digit4());
+    GeneratedPin.value = digit4();
+    GeneratedPinValue = GeneratedPin.value;
+    console.log(GeneratedPinValue);
+}
+
+// let digitBtn = document.getElementsByClassName("button");
+let digits = document.getElementById("digits");
+function digitInput(digit) {
+    document.getElementById(digit).addEventListener("click", function() {
+        console.log("cliked " + digit);
+        digits.value = digits.value + parseInt(digit);
+    });
+    digitsValue = digits.value;
+    console.log(digitsValue);
+}
+
+function matchBtn() {
+    if(GeneratedPinValue === digitsValue) {
+        console.log("Match");
+        document.getElementById("ok").style.display = "block"
+    }
 }
